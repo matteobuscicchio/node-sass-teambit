f7eb392b60b9810688e898b67f4e15c85be95574
matteobuscicchio <matteoezio@me.com>
2021-01-27 18:00:39 +0100

    footer completato

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 let app = new Vue({
     el:'#app',
-    data:{},
+    data:{
+
+        // sezione: FOOTER
+        footer:{
+            linkList:[
+                {
+                    title:'product',
+                    links:['Feedback','Surveys','Reviews','praise','Slack','Integration','Schedule a demo','pricing'],
+                },
+                {
+                    title:'about',
+                    links:['Feedback','Surveys','Reviews','praise'],
+                },
+                {
+                    title:'connect',
+                    links:['Feedback','Surveys','Reviews','praise'],
+                },
+                {
+                    title:'lobs',
+                    links:['Feedback','Surveys','Reviews','praise'],
+                },
+                {
+                    title:'from the blog',
+                    links:['How to change your company culture: a four-step framework','How teams can be more productive by killing 8-hour workday','How to build high performing teams inspired by nature all'],
+                    // links:['lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem','lorem'],
+                },
+            ],
+        },
+    },
 });
